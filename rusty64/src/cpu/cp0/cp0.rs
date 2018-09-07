134568ed77fb7071ac259f9cf379fe280c61522b
Nettozx <nettomajid@gmail.com>
2018-09-07 12:40:06 -0700

    RegConfig and RegStatus complete

--- rusty64/src/cpu/cp0/cp0.rs
+++ rusty64/src/cpu/cp0/cp0.rs
@@ -1,11 +1,12 @@
-use super::reg_config;
 use super::reg_status;
+use super::reg_config;
 
 #[derive(Default, Debug)]
 pub struct Cp0 {
     //page 46 on datasheet & chapter 5
-    reg_config: reg_config::RegConfig,
-    reg_status: reg_status::RegStatus
+    reg_status: reg_status::RegStatus,
+    reg_config: reg_config::RegConfig
+
 }
 
 impl Cp0 {
@@ -16,13 +17,11 @@ impl Cp0 {
 
     pub fn write_reg(&mut self, index: u32, data: u64) {
         match index {
-            12 => { self.write_status_reg(data); }
-            _ => panic!("Unrecognized Cp0 reg: {:#?}, {:#?}", index, data)
+            // Status reg - section 6.3.5 in datasheet
+            12 => { self.reg_status = (data as u32).into(); },
+            // Config reg - section 5.4.6 in datasheet
+            16 => { self.reg_config = (data as u32).into(); }
+            _ => panic!("Unrecognized Cp0 reg: {}, {:#x}", index, data)
         }
-    }
-
-    fn write_status_reg(&mut self, data: u64) {
-        // Status reg - section 6.3.5 in datasheet
-        self.reg_status.write(data as u32);
     }
 }
--- rusty64/src/cpu/cp0/reg_config.rs
+++ rusty64/src/cpu/cp0/reg_config.rs
@@ -1,37 +1,82 @@
 #[derive(Debug)]
-enum Ep { //page153 on datasheet
-    D,
-    DxxDxx,
-    RFU
+enum DataTransferPattern { //page153 on datasheet
+    Normal, //D
+    DxxDxx, //DxxDxx
+    //RFU //RFU - reserved for future use
 }
 
-impl Default for Ep {
+impl Default for DataTransferPattern {
     fn default() -> Self {
-        Ep::D
+        DataTransferPattern::Normal
+    }
+}
+
+impl From<u32> for DataTransferPattern {
+    fn from(value: u32) -> Self {
+        match (value >> 24) & 0b1111 {
+            0 => DataTransferPattern::Normal,
+            6 => DataTransferPattern::DxxDxx,
+            _ => panic!("Invalid data transfoer pattern (EP): {:#x}", value)
+        }
     }
 }
 
 #[derive(Debug)]
-enum Be { //section 5.4.6
-    LittleEndian,
-    BigEndian
+enum Endianness { //section 5.4.6
+    Little,
+    Big
 }
 
-impl Default for Be {
+impl Default for Endianness {
     fn default() -> Self {
-        Be::BigEndian
+        Endianness::Big
+    }
+}
+
+impl From<u32> for Endianness {
+    fn from(value: u32) -> Self {
+        match (value >> 15) & 0b1 {
+            0 => Endianness::Little,
+            1 => Endianness::Big,
+            _ => unreachable!()
+        }
     }
 }
 
 #[derive(Default, Debug)]
 pub struct RegConfig {
-    ep: Ep,
-    be: Be
+    //EP
+    data_transfer_pattern: DataTransferPattern,
+    //BE
+    endianness: Endianness,
+    //CU
+    cu: bool,
+    //K0
+    kseg0_cache_enabled: bool
 }
 
 impl RegConfig {
     pub fn power_on_reset(&mut self) {
-        self.ep = Ep::D;
-        self.be = Be::BigEndian;
+        self.data_transfer_pattern = DataTransferPattern::Normal;
+        self.endianness = Endianness::Big;
+    }
+}
+
+impl From<u32> for RegConfig {
+    fn from(value: u32) -> Self {
+        //page 152 in datasheet
+        RegConfig {
+            //0 preset, EC unused
+            // EP
+            data_transfer_pattern: value.into(),
+            //00000110 preset
+            //BE
+            endianness: value.into(),
+            //11001000110 preset
+            //CU
+            cu: (value & (1 << 3)) != 0,
+            //K0
+            kseg0_cache_enabled: value & 0b111 != 0b010
+        }
     }
 }
