ee86d30ee2bba4a077388b897d1ce77882e2fd91
Nettozx <nettomajid@gmail.com>
2018-09-06 20:18:38 -0700

    reorganizing code
    at this point you can print out cpu registers

--- rusty64/src/main.rs
+++ rusty64/src/main.rs
@@ -1,82 +1,9 @@
+mod cpu;
+
 use std::env;
 use std::fs;
 use std::io::Read;
 use std::path::Path;
-
-const NUM_GPR: usize = 32; //number of general purpose registers
-
-#[derive(Default, Debug)]
-struct Cpu {
-    //section 1.4.2 CPU Registers on datasheet
-    reg_gpr: [u64; NUM_GPR],
-    reg_fpr: [f64; NUM_GPR],
-
-    reg_pc: u64,
-
-    reg_hi: u64,
-    reg_lo: u64,
-
-    reg_llbit: bool, //TODO: Enum type
-
-    reg_fcr0: u32,
-    reg_fcr31: u32,
-
-    cp0: Cp0,
-}
-
-impl Cpu {
-    fn new() -> Cpu {
-        Cpu::default()
-    }
-
-    fn power_on_reset(&mut self) {
-        self.cp0.power_on_reset();
-    }
-}
-
-#[derive(Debug)]
-enum RegConfigEp { //page153 on datasheet
-    D,
-    DxxDxx,
-    RFU
-}
-
-impl Default for RegConfigEp {
-    fn default() -> RegConfigEp {
-        RegConfigEp::D
-    }
-}
-
-#[derive(Debug)]
-enum RegConfigBe { //section 5.4.6
-    LittleEndian,
-    BigEndian
-}
-
-impl Default for RegConfigBe {
-    fn default() -> RegConfigBe {
-        RegConfigBe::BigEndian
-    }
-}
-
-#[derive(Default, Debug)]
-struct Cp0 {
-    //page 46 on datasheet & chapter 5
-    reg_config_ep: RegConfigEp,
-    reg_config_be: RegConfigBe
-}
-
-impl Cp0 {
-    fn new() -> Cp0 {
-        Cp0::default()
-    }
-
-    fn power_on_reset(&mut self) {
-        //section 9.2.1 in datasheet & page 153
-        self.reg_config_ep = RegConfigEp::D;
-        self.reg_config_be = RegConfigBe::BigEndian;
-    }
-}
 
 fn main() {
     //N64 Specs
@@ -122,10 +49,10 @@ fn main() {
     let rom = read_bin(rom_file_name);
 
     //define cpu
-    let mut cpu = Cpu::new();
+    let mut cpu = cpu::Cpu::default();
     //reset cpu
     cpu.power_on_reset();
-    println!("{:?}", &cpu);
+    println!("{:#?}", &cpu);
 }
 
 fn read_bin<P: AsRef<Path>>(path: P) -> Vec<u8> {
--- rusty64/target/debug/deps/rusty64-64de35c79b9661f7.d
+++ rusty64/target/debug/deps/rusty64-64de35c79b9661f7.d
@@ -1,5 +1,6 @@
-D:\Users\Netto\Documents\GitHub\rusty64\rusty64\target\debug\deps\rusty64-64de35c79b9661f7.exe: src\main.rs
+D:\Users\Netto\Documents\GitHub\rusty64\rusty64\target\debug\deps\rusty64-64de35c79b9661f7.exe: src\main.rs src\cpu.rs
 
-D:\Users\Netto\Documents\GitHub\rusty64\rusty64\target\debug\deps\rusty64-64de35c79b9661f7.d: src\main.rs
+D:\Users\Netto\Documents\GitHub\rusty64\rusty64\target\debug\deps\rusty64-64de35c79b9661f7.d: src\main.rs src\cpu.rs
 
 src\main.rs:
+src\cpu.rs:
